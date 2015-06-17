fd0d2baf1ae749ed01cec14a4f36bdf3ef4fa8a9
altsheets <altsheets+coding@gmail.com>
2015-06-17 12:32:06 +0000

    server preparations

--- prepareServer.sh
+++ prepareServer.sh
@@ -1,2 +1,13 @@
+echo # dependencies
 apt-get install apache2 php5 php5-sqlite php5-curl sqlite3
 
+echo # change port 80 to 8080 in
+echo nano /etc/apache2/ports.conf
+echo nano /etc/apache2/sites-enabled/000-default
+echo /etc/init.d/apache2 restart
+
+echo # mv b.e. folder into www-folder 
+echo mv hz-blockexplorer/hz-blockexplorer /var/www/hzbe 
+
+echo # move (and perhaps edit) the config file
+echo mv /var/www/hzbe/config/config.php.default /var/www/hzbe/config/config.php
